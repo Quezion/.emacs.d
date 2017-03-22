770ca581d6fe761ab7517f0ef90ea246c361d60c
Quest Yarbrough <quest.yarbrough@concur.com>
2017-03-22 11:22:35 -0700

    Added Paredit, Ido, Aggressive-Indent, and various settings+fns

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 ;; MELPA (latest)
 ;; https://www.emacswiki.org/emacs/MELPA
 (when (>= emacs-major-version 24)
@@ -7,7 +8,20 @@
    '("melpa" . "http://melpa.org/packages/")
    t)
   (package-initialize))
-;; (package-refresh-contents) ;; Required to maintain updated package list
+;; IF YOU ARE HAVING DIFFICULTIES GETTING PACKAGES, UNCOMMENT BELOW LINE
+;; It's necessary any time I add a new package or install for the first time
+(package-refresh-contents) ;; Required to maintain updated package list
+
+;; Makes Emacs remember open buffers between runs
+(desktop-save-mode 1)
+
+;; Disable logging messages when font-locking to speed up rendering
+(setq font-lock-verbose nil)
+
+;; Ido mode -- enables automatic matching for buffers and files as you type
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
 
 ;; Required for Emacs to find lein
 (add-to-list 'exec-path "/usr/local/bin")
@@ -21,13 +35,36 @@
   (package-install 'rainbow-delimiters))
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
+;; Aggressive Indent Mode - https://github.com/Malabarba/aggressive-indent-mode
+(unless (package-installed-p 'aggressive-indent)
+  (package-install 'aggressive-indent))
+(add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+(add-hook 'css-mode-hook #'aggressive-indent-mode)
+(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
+
+;; Paredit for major LISP modes - https://www.emacswiki.org/emacs/ParEdit
+(unless (package-installed-p 'paredit)
+  (package-install 'paredit))
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
 ;; CIDER - https://github.com/clojure-emacs/cider
 (unless (package-installed-p 'cider)
   (package-install 'cider))
+(add-hook 'cider-mode-hook #'eldoc-mode)
 
 ;; Solarized-theme - https://github.com/bbatsov/solarized-emacs
 (unless (package-installed-p 'solarized-theme)
   (package-install 'solarized-theme))
+
+(unless (package-installed-p 'desktop+)
+  (package-install 'desktop+))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -38,7 +75,9 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(package-selected-packages (quote (solarized-theme rainbow-delimiters clojure-mode))))
+ '(package-selected-packages
+   (quote
+    (aggressive-indent clj-refactor solarized-theme rainbow-delimiters clojure-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -48,3 +87,16 @@
 
 (load-theme 'solarized-dark)
 
+;; Custom functions
+(defun close-all-buffers ()
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
+
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
+	(revert-buffer t t t) )))
+  (message "Refreshed open files."))
