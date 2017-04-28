7c1e7f2db1125cf0ec552883a2a73a66f9ff5e52
Quest Yarbrough <quest.yarbrough@concur.com>
2017-04-28 11:35:28 -0700

    Dockerfile support, Clojure Cats Monads support

--- init.el
+++ init.el
@@ -11,6 +11,8 @@
 ;; It's necessary any time I add a new package or install for the first time
 (package-refresh-contents) ;; Required to maintain updated package list
 
+;; TODO: fix colors being off in from `ansi-term` command
+
 ;; Makes Emacs remember open buffers between runs
 (desktop-save-mode 1)
 
@@ -38,6 +40,12 @@
 ;; Clojure mode - https://github.com/clojure-emacs/clojure-mode
 (unless (package-installed-p 'clojure-mode)
   (package-install 'clojure-mode))
+
+;; Editor intg for cats monad library - http://funcool.github.io/cats/latest/#editor-integration
+(require 'clojure-mode)
+(define-clojure-indent
+  (alet 'defun)
+  (mlet 'defun))
 
 ;; Extra font locking (keyword highlighting) for Clojure/CLJS
 (unless (package-installed-p 'clojure-mode-extra-font-locking)
@@ -85,6 +93,30 @@
 (unless (package-installed-p 'desktop+)
   (package-install 'desktop+))
 
+;; yaml-mode to provide YAML syntax support
+(unless (package-installed-p 'yaml-mode)
+  (package-install 'yaml-mode))
+
+;; docker.el - https://github.com/Silex/docker.el
+;; enables docker commands from Emacs
+;; try M-x docker-images
+(unless (package-installed-p 'docker)
+  (package-install 'docker))
+
+;; OSX only Docker configuration, will probably break Emacs on Linux
+;; (by default Linux installs docker to /bin/bash/docker, but OSX does not)
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+;; Use "docker-machine env box" command to find out your environment variables
+(setenv "DOCKER_TLS_VERIFY" "1")
+(setenv "DOCKER_HOST" "tcp://10.11.12.13:2376")
+(setenv "DOCKER_CERT_PATH" "/Users/foo/.docker/machine/machines/box")
+(setenv "DOCKER_MACHINE_NAME" "box")
+
+;; syntax highlighting for Dockerfiles
+(unless (package-installed-p 'dockerfile-mode)
+  (package-install 'dockerfile-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -97,7 +129,7 @@
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
  '(package-selected-packages
    (quote
-    (ido-at-point-mode ido-vertical-mode visual-regexp-steroids clojure-mode-extra-font-locking aggressive-indent clj-refactor solarized-theme rainbow-delimiters clojure-mode))))
+    (yaml-mode dockerfile-mode docker ido-at-point-mode ido-vertical-mode visual-regexp-steroids clojure-mode-extra-font-locking aggressive-indent clj-refactor solarized-theme rainbow-delimiters clojure-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
