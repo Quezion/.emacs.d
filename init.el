1dea3319dbe174c92326f7f88eef024260b5cfb2
Quest Yarbrough <quest@yarbrough.net>
2018-05-23 15:35:42 -0700

    Replaced C-x 2/3 "split buffer" to automatic open next buffer in list

--- init.el
+++ init.el
@@ -326,6 +326,22 @@ the current position of point, then move it to the beginning of the line."
     (when (eq pt (point))
       (beginning-of-line))))
 
+;; Automatically switch to next buffer when opening a new buffer window (C-x 2/3)
+(defun vsplit-last-buffer ()
+  (interactive)
+  (split-window-vertically)
+  (other-window 1 nil)
+  (switch-to-next-buffer))
+
+(defun hsplit-last-buffer ()
+  (interactive)
+  (split-window-horizontally)
+  (other-window 1 nil)
+  (switch-to-next-buffer))
+
+(global-set-key (kbd "C-x 2") 'vsplit-last-buffer)
+(global-set-key (kbd "C-x 3") 'hsplit-last-buffer)
+
 ;; ************************************
 ;; ---~~~=======  KEYBINDS  =======~~~---
 ;; ************************************
